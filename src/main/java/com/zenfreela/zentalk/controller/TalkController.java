df85d3efcb280e57aa602120204ee509899a9338
JPereirax <jailsondev70@gmail.com>
2019-03-22 23:50:36 -0300

    feat: :sparkles: add 'findByEmail'.

--- src/main/java/com/zenfreela/zentalk/controller/TalkController.java
+++ src/main/java/com/zenfreela/zentalk/controller/TalkController.java
@@ -4,10 +4,14 @@ import com.zenfreela.zentalk.model.Talk;
 import com.zenfreela.zentalk.repository.TalkRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.lang.NonNull;
+import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.springframework.http.ResponseEntity.*;
 
@@ -25,6 +29,28 @@ public class TalkController {
     @GetMapping(path = "", produces = "application/json")
     public ResponseEntity<Flux<Talk>> findAll() {
         return ok(talkRepository.findAll());
+    }
+
+    @GetMapping(path = "/{email}", produces = "application/json")
+    public ResponseEntity<Flux<Talk>> findByEmail(@PathVariable("email") @NonNull String email) {
+        List<Talk> talkList = talkRepository.findAll().collectList().block();
+        if (talkList == null) {
+            return notFound().build();
+        }
+
+        if (talkList.size() == 0) {
+            return noContent().build();
+        }
+
+        List<Talk> memberTalk = talkList.stream()
+                .filter($ -> $.getMembers().contains(email))
+                .collect(Collectors.toCollection(LinkedList::new));
+
+        Flux<Talk> talkFlux = Flux.fromArray(
+                memberTalk.toArray(new Talk[0])
+        );
+
+        return ok(talkFlux);
     }
 
 }
