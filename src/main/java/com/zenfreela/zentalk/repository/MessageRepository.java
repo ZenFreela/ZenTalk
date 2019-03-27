54dccefeaf7c89c07a54cc0d5ac68822acf1034a
JPereirax <jailsondev70@gmail.com>
2019-03-26 22:11:04 -0300

    feat: :sparkles: add 'message' service.

--- src/main/java/com/zenfreela/zentalk/repository/MessageRepository.java
+++ src/main/java/com/zenfreela/zentalk/repository/MessageRepository.java
@@ -2,8 +2,10 @@ package com.zenfreela.zentalk.repository;
 
 import com.zenfreela.zentalk.model.Message;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import org.springframework.stereotype.Repository;
 import reactor.core.publisher.Flux;
 
+@Repository
 public interface MessageRepository extends ReactiveMongoRepository<Message, String> {
 
     Flux<Message> findBySender(String sender);
